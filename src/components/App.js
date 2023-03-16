38bfe468ab894f4392d2597a906635fb282121f5
DEVENDRA MESHRAM <120394539+triggerdeva@users.noreply.github.com>
2023-03-16 21:03:02 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,19 @@ import React,{useState,useEffect} from 'react'
 import '../styles/App.css';
 const App = () => {
 //code here
+const[name, setName]=useState("");
+const[printName, setPrintName]=useState(false)
+const handleChange=(e)=>{
+  setName(e.target.value);
+}
+const fixOutput = ()=>{
+  setPrintName(true);
+}
   return (
     <div id="main">
-      <input id='input' onChange={}></input>
-      <button id='button' onClick={}>Click</button>
-      <p id='text'> Hello my name is ____ and I study at Newton School</p>
+      <input id='input' onChange={handleChange}></input>
+      <button id='button' onClick={fixOutput}>Click</button>
+      <p id='text'> Hello my name is {printName? name : "____"} and I study at Newton School</p>
     </div>
   )
 }
